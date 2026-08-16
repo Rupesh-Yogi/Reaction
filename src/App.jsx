2b9f7982ef57b0296a6544257c2d77e129881324
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-08-16 08:44:58 +0100

    learned jsx attribute

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 // function App() {
-//   const price = 100;
-//   const quantity = 3;
+//   const imageUrl = "https://picsum.photos/300";
 
 //   return (
 //     <div>
-//       <h1>Shopping Cart</h1>
-//       <p>Price: £{price}</p>
-//       <p>Quantity: {quantity}</p>
-//       <p>Total: £{price * quantity}</p>
+//       <h1>My Image</h1>
+//       <img src={imageUrl} alt="Random image" />
 //     </div>
 //   );
 // }
@@ -15,22 +12,15 @@
 // export default App;
 
 
-
-
-
 function App() {
-  const paisa = 40;
-  const katiSaman = 4;
-
+  const imgUrl = "https://imgs.search.brave.com/qtGUeI6nlGllqggCEYHq2yQfLaXquaIWktPfhHKqYWU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy93b21hbi13aXRo/LXN1bi1nbGFzc2Vz/LWluLWZsb3dlci1m/aWVsZC1zdW1tZXIt/ZnJlZS1waG90by5q/cGc_dz02MDAmcXVh/bGl0eT04MA";
 
   return(
     <div>
-      <h1>Shopping ko paisa</h1>
-      <p>Paisa : £{paisa}</p>
-      <p>Kati Ota Saman : {katiSaman}</p>
-      <p>Total Kharcha : £{paisa * katiSaman}</p>
+      <h1>Naya Image</h1>
+      <img src={imgUrl} alt="Picture of beautiful girl" />
     </div>
-  )
+  );
 }
 
 export default App;

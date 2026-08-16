9574747d697263624f752aa330c8a13d84fd02a7
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-08-16 08:37:53 +0100

    learned about jsx expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-function App() {
-  const name = "Rupesh";
-  const age = 22;
+// function App() {
+//   const price = 100;
+//   const quantity = 3;
 
-  return (
+//   return (
+//     <div>
+//       <h1>Shopping Cart</h1>
+//       <p>Price: £{price}</p>
+//       <p>Quantity: {quantity}</p>
+//       <p>Total: £{price * quantity}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+function App() {
+  const paisa = 40;
+  const katiSaman = 4;
+
+
+  return(
     <div>
-      <h1>Hello {name}</h1>
-      <p>Age: {age}</p>
+      <h1>Shopping ko paisa</h1>
+      <p>Paisa : £{paisa}</p>
+      <p>Kati Ota Saman : {katiSaman}</p>
+      <p>Total Kharcha : £{paisa * katiSaman}</p>
     </div>
-  );
+  )
 }
 
 export default App;

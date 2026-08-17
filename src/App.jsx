6fb76dce743036a0d4e1f44a0f4f1c855f846a38
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-08-17 08:04:37 +0100

    learned map, key and body function

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-// function App() {
-//   const isLoggedIn = true;
-
-//   return (
-//     <div>
-//       <h1>My Website</h1>
-
-//       {isLoggedIn && <p>Welcome back!</p>}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 function App() {
-  const isLoggedIn = false;
+  const fruits = ["banana", "apple", "Mango", "Orange"];
 
   return (
     <div>
-
-    <h1>My first React Website</h1>
-    {/* {isLoggedIn && <p>Welcome!</p>} */}
-    {isLoggedIn ? <p>Welcome!</p> : <p>Please Log in first.</p>}
+      {fruits.map((fruit) => {
+        return <li key={fruit}>{fruit}</li>;
+      })}
     </div>
-  )
+  );
 }
 
 export default App;

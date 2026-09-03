53fcbae9d7dc38b958e3f49ca33763b3f80f2d05
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-09-03 07:18:54 +0100

    add student list using props and map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
-function App() {
-  const fruits = ["banana", "apple", "Mango", "Orange"];
-
+function StudentCard(props) {
   return (
-    <div>
-      {fruits.map((fruit) => {
-        return <li key={fruit}>{fruit}</li>;
-      })}
-    </div>
+            <>
+          <h1>Student Card</h1>
+          <p>{props.students.name}</p>
+          <p>Age: {props.students.age}</p>
+          <p>Attendance Status : {props.students.isPresent ? "Present" : "Absent"} </p>
+          <p>{props.students.isPresent && <>Present Today</>}</p>
+        </>
   );
+}
+
+function App() {
+  const students = [
+    {
+      name: "Rupesh",
+      age: 22,
+      course: "Wev Development",
+      isPresent: true
+    },
+
+    {
+      name: "Alex",
+      age: 33,
+      course: "Machine Learning",
+      isPresent: false
+    },
+
+    {
+      name: "Jhon",
+      age: 24,
+      course: "Software Engineering",
+      isPresent: false
+    },
+
+    {
+      name: "Pramish",
+      age: 25,
+      course: "Business Studies",
+      isPresent: true
+    },
+  ];
+
+  return(
+    <>
+    {students.map((student) => (
+          <StudentCard key={student.name} students = {student} />
+
+    ))}
+
+    </>
+  )
 }
 
 export default App;

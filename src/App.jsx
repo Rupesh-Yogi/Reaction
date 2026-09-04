a908a28313a5b156354e9ff1f490fb75b6ffa146
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-09-05 00:02:41 +0100

    learned how to pass function into child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,35 @@
-function StudentCard({students}) {
-  return (
-    <>
-      <h1>Student Card</h1>
-      <p>{students.name}</p>
-      <p>Age: {students.age}</p>
-      <p>
-        Attendance Status :{" "}
-        {students.isPresent ? "Present" : "Absent"}{" "}
-      </p>
-      <p>{students.isPresent && <>Present Today</>}</p>
-    </>
-  );
+// function MyButton({onClick}) {
+
+//   return <button onClick = {onClick}>Click here</button>
+// }
+
+// function App() {
+
+//     function showMessage(){
+//       console.log("Button Clicked!");
+//     }
+
+//     return <MyButton onClick = {showMessage}/>
+// }
+
+// export default App;
+
+
+
+function MyButton({clccl}){
+
+  return <button onClick = {clccl}> Click Me</button>
 }
 
-function App() {
-  const students = [
-    {
-      name: "Rupesh",
-      age: 22,
-      course: "Web Development",
-      isPresent: true,
-    },
+function App(){
 
-    {
-      name: "Alex",
-      age: 33,
-      course: "Machine Learning",
-      isPresent: false,
-    },
-
-    {
-      name: "Jhon",
-      age: 24,
-      course: "Software Engineering",
-      isPresent: false,
-    },
-
-    {
-      name: "Pramish",
-      age: 25,
-      course: "Business Studies",
-      isPresent: true,
-    },
-  ];
+  function showMessage(){
+    console.log("Something done with button.")
+  }
 
   return (
-    <>
-      {students.map((student) => (
-        <StudentCard key={student.name} students={student} />
-      ))}
-    </>
-  );
+    <MyButton  clccl = {showMessage} />
+  )
 }
 
 export default App;
-
-
